37b50cbecbb4dfb70cefd3ddae1c2e2d7afe7255
Garrett Adams <garrett.adams52@gmail.com>
2022-06-08 16:10:12 -0400

    finished simplified

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,7 +1,23 @@
 sequence = [4, 3, 5, 0, 1]
 swaps = 0
+run = True
+result = []
 
 # Your Code Here
+while run:
+    run = False
+    for i, x in enumerate(sequence):
+        if i == len(sequence)-1:
+            break
+        if sequence[i] > sequence[i+1]:
+            temp = sequence[i]
+            sequence[i] = sequence[i+1]
+            sequence[i+1] = temp
+            run = True
+            swaps += 1
+        #print(sequence,run)
+
+result = sequence
 
 print(f"Final result: {result}")
 print(f"Swaps: {swaps}")
